2b9a10e8687c6279a9012c3f74901810e0cdf461
ezgiz <ezgiz@MSI>
2024-05-14 20:06:31 +0300

    ..

---
+++ RealEstateDapperUI/ViewComponents/HomePage/_DefaultDiscountOfDayComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstateDapperUI.ViewComponents.HomePage
+{
+    public class _DefaultDiscountOfDayComponentPartial :ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
