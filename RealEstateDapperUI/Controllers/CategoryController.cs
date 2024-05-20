9f7ca8b23443c0c583cd8876651513c072866482
ezgiz <ezgiz@MSI>
2024-05-20 23:36:46 +0300

    ..

---
+++ RealEstateDapperUI/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstateDapperUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
