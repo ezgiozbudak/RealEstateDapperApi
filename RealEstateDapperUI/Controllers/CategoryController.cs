dc3f9fe5b2d824b28145f1e68156dcb5dc12197e
ezgiz <ezgiz@MSI>
2024-05-21 12:25:52 +0300

    admin layout güncelleştirmeleri

--- RealEstateDapperUI/Controllers/CategoryController.cs
+++ RealEstateDapperUI/Controllers/CategoryController.cs
@@ -1,11 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstateDapperUI.Dtos.CategoryDtos;
 
 namespace RealEstateDapperUI.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoryController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7267/api/Categories");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+
             return View();
         }
     }
